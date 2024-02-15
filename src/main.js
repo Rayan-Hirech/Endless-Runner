44d2a3e6fb08cf2ed50e7a768b7a52748646051e
Rayan Hirech <ramine411@gmail.com>
2024-02-14 19:46:07 -0800

    Added creative tilt justification.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@
 Name: Rayan Hirech
 Game Title: Teleport Egg
 Hours Spent: 22
-Creative Tilt:
+Creative Tilt: I incorporated two unique elements to make my game creative.
+    First, the obstacles move in an arc instead of a straight line, to simulate an egg's curved rolling path.
+        To accomplish this, I used trigonometry to calculate the position the obstacle would be along a circle given it's angular velocity.
+        In terms of gameplay, it becomes more challenging to predict the trajectory of an obstacle, since different radii lead to different velocities.
+    Second, the player has to charge a teleport to move their character.
+        This way, the player has to eyeball how much to charge to go a certain distance.
+        The player has to strategically wait for the right time to start charging, so they don't charge for too little or too long.
+        In addition, there is a short cooldown on teleportation, so players can't just spam it.
 */
 
 let config = {
